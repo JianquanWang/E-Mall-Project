1cfdb33f94bffd57567067d054c7f03ff65b09a4
JianquanWang <jwang438@uottawa.ca>
2020-07-17 20:29:23 -0400

    Create product's entity

--- src/tech/oldwang/domain/Product.java
+++ src/tech/oldwang/domain/Product.java
@@ -1,5 +1,63 @@
 package tech.oldwang.domain;
 
 public class Product {
-
+	private int pid;
+	private String pname;
+	private String author;
+	private float price;
+	private String description;
+	private String filename;
+	private String path;
+	// Category id
+	private Category category = new Category();
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
 }
